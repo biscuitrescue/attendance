da046f7e93e94e5a19f942226527782e0e4590f1
biscuitrescue <karttikeyasinha11@gmail.com>
2024-08-09 02:40:57 +0530

    struct

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,29 @@ use std::ops::Add;
 
 fn main() {
     trait Classes {
-    fn attendance(total: f32, attended: f32) -> f32 {
-        (attended / total) * 100.0
-    }
+        fn new(name: String, rollno: i32, total: f32, attended: f32) -> Self;
+        fn attendance(&self) -> f32;
     }
     struct Student {
+        name: String,
         rollno: i32,
+        total: f32,
+        attended: f32
     }
+
+    impl Classes for Student {
+        fn new(name: String, rollno: i32, total: f32, attended: f32) -> Student{
+            Student{name, rollno, total, attended}
+        }
+        fn attendance(&self) -> f32 {
+            (self.attended / self.total) * 100.0
+        }
+    }
+
+    let path = std::path::Path::new("attendance.txt");
+    let path = std::path::Path::new("attendance.txt");
+
+    let stud2 = <Student as Classes>::new("Karttikeya".to_string(), 102313034, 35.0, 19.0);
+    // println!("Attendance is {}", stud2.attendance());
+
 }
